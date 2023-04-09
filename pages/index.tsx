d00fa3228f35708d2ad3ef86827129b499fa05b0
ismailsindhi <muhammadismailsoomro09@gmail.com>
2023-04-09 23:55:08 +0500

    sindhi added

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/maet.jpeg" />
       </Head>
-      <div className="flex flex-col items-center mt-4 mx-auto gap-4 border-2 max-w-md py-5">
+      <div className="flex flex-col items-center mt-4 mx-auto gap-4 max-w-xs py-5">
       <Image src="/maet.jpeg" alt="Maet Group Logo" width={180} height={180} />
       <h1 className="text-blue-500 text-2xl font-bold">Welcome To Maet Group</h1>
+      <p className="text-blue-500 text-xl font-verctex text-center border-b-2 pb-2">پنھنجو نالو ولديت لکي ڪري ھيٺئين بٽن کي ڪلڪ ڪري مائٽ گروپ ۾ شامل ٿيو.</p>
       <InputField placeholder="Name" />
       <InputField placeholder="Fathers Name" />
       <SubmitButton text="Join Maet Group" />
