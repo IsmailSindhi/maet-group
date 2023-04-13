734017493fe330e882490af928357ae6a0c9b1f8
ismailsindhi <muhammadismailsoomro09@gmail.com>
2023-04-13 22:49:07 +0500

    vidation done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,53 @@
 import Head from "next/head";
 import Image from "next/image";
-
-import InputField from "@/components/inputfiled";
+import { useRouter } from 'next/navigation';
 import SubmitButton from "@/components/button";
 import React, { useState, createContext, FormEvent } from "react";
 import { type } from "os";
-import { useRouter } from 'next/router';
-type AppContextType = {
+import {z, ZodType} from 'zod';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type FormData = {
   name: string;
-  setName: any;
   fathername: string;
-  setFathername: any;
   contact: string;
-  setContact: any;
 };
 
-export const AppContext = createContext<AppContextType>({
-  name: "",
-  setName: () => {},
-  fathername: "",
-  setFathername: () => {},
-  contact : "",
-  setContact: () => {},
-});
 export default function Home() {
   // functionality
-  // State
   const router = useRouter();
-  const [name, setName] = useState<string>("");
-  const [fathername, setFathername] = useState<string>("");
-  const [contact, setContact] = useState<string>("");
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const schema: ZodType<FormData> = z
+    .object({
+      name: z.string().min(3).max(30),
+      fathername: z.string().min(3).max(30),
+      contact: z.string().min(10),
+    });
+    const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors },
+    } = useForm<FormData>({
+      resolver: zodResolver(schema),
+    });
+    const submitData = (data: FormData) => {
+      console.log("IT WORKED", data);
+      reset();
+      const currentDate = new Date();
+      const formattedDateTime = currentDate.toLocaleString();
+      
+      
+      const { name, fathername, contact} = data ;
+      const form = { name, fathername, contact, formattedDateTime};
+      const response = fetch('api/submit', {method: 'POST' , headers : {'Accept' : 'application/json', 'Content-Type' : 'application/json'}, body: JSON.stringify(form)})
 
-    const currentDate = new Date();
-    const formattedDateTime = currentDate.toLocaleString();
-    const form = { name, fathername, contact, formattedDateTime};
-    console.log(formattedDateTime);
-    const response = fetch('api/submit', {method: 'POST' , headers : {'Accept' : 'application/json', 'Content-Type' : 'application/json'}, body: JSON.stringify(form)})
-    console.log(response);
-    // const content = await response.json();
-    // alert(content.data.tableRange);
-    setName("")
-    setFathername("")
-    setContact("")
-    router.push('https://chat.whatsapp.com/DsdZctuoD4o75IxSHGMyw0');
-  };
+      router.push('https://chat.whatsapp.com/DsdZctuoD4o75IxSHGMyw0');
 
-  // UI`
+
+    };
+  
+
   return (
     <>
       <Head>
@@ -59,7 +59,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/maet.jpeg" />
       </Head>
-      <AppContext.Provider value={{ name, setName, fathername, setFathername, contact, setContact }}>
         <div className="flex flex-col items-center mt-4 mx-auto gap-4 max-w-xs py-5 bg-white">
           <Image
             src="/maet.jpeg"
@@ -70,30 +69,29 @@ export default function Home() {
           <h1 className="text-blue-500 text-2xl font-bold">
             Welcome To Maet Group
           </h1>
-          <p className="text-blue-500 text-xl font-verctex text-center border-b-2 pb-2">
-            پنھنجو نالو، ولديت  ۽ ڪانٽيڪٽ نمبر لکي ڪري ھيٺئين بٽڻ کي ڪلڪ ڪري مائٽ گروپ ۾ شامل ٿيو.<span className="text-white">س</span>
-          
-          
+          <p className="text-blue-500 text-xl font-verctex text-center border-b-2 pb-2 select-none">
+            پنھنجو نالو،  ولديت  ۽ ڪانٽيڪٽ نمبر لکي ڪري ھيٺئين بٽڻ کي ڪلڪ ڪري مائٽ گروپ ۾ شامل ٿيو.<span className="text-white select-none text-[1px]">س</span>
           </p>
-          <form className="flex flex-col items-center justify-center gap-4" onSubmit={handleSubmit}>
-            <InputField placeholder="Name" value={name} setValue={setName} type="text"/>
-            <InputField
+          <form className="flex flex-col items-center justify-center gap-4" onSubmit={handleSubmit(submitData)}>
+            <input className="bg-blue-100 text-blue-700 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:bg-white" placeholder="Name" {...register("name")} type="text"/>
+            {errors.name && <span className="text-rose-500">Must contain at least 3 characters</span>}
+            <input
               placeholder="Father's Name"
-              value={fathername}
-              setValue={setFathername}
               type="text"
+              className="bg-blue-100 text-blue-700 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:bg-white"
+              {...register("fathername")}
               />
-              {/* <input placeholder="Contact" value={contact}  */}
-            <InputField
+              {errors.fathername && <span className="text-rose-500"> Must contain at least 3 characters</span>}
+            <input
               placeholder="Phone Number"
-              value={contact}
-              setValue={setContact}
-              type="tel"
-            />
+              type="number"
+              className="bg-blue-100 text-blue-700 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:bg-white"
+              {...register("contact")}
+              />
+              {errors.contact && <span className="text-rose-500"> Must contain at least 11 digits</span>}
             <SubmitButton text="Join Maet Group" />
           </form>
         </div>
-      </AppContext.Provider>
     </>
   );
 }
